6e839f0212f6eab6792693ea5033944c80397f57
Facundo <fastiz@Araucaria-2.local>
2020-03-18 19:38:10 -0300

    added list of simulations to be run n amount of times and with 2000, 3000  and 5000 particles

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,37 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args){
+        final int NUMBER_OF_ITERATIONS = 3000;
+        final int NUMBER_OF_EXECUTIONS = 20;
 
-        try{
-            TwoContainersSimulation tcs = new TwoContainersSimulation(200, 200, 2000,
-                    1000, "staticFile", "dynamicFile", "particleDistributionFile", 10);
-        }catch (IOException | NotEnoughSpaceException e){
-            System.err.println(e);
-            return;
+        for(int i =0; i<NUMBER_OF_EXECUTIONS; i++) {
+            System.out.println("Running ("+(i+1)+"/"+NUMBER_OF_EXECUTIONS+")");
+            try {
+                String folder = "particles2000/";
+                TwoContainersSimulation tcs = new TwoContainersSimulation(200, 200, 2000,
+                        NUMBER_OF_ITERATIONS, folder + "staticFile", folder + "dynamicFile", folder + "particleDistributionFile" + i, 10);
+            } catch (IOException | NotEnoughSpaceException e) {
+                System.err.println(e);
+                return;
+            }
+
+            try {
+                String folder = "particles3000/";
+                TwoContainersSimulation tcs = new TwoContainersSimulation(200, 200, 3000,
+                        NUMBER_OF_ITERATIONS, folder + "staticFile", folder + "dynamicFile", folder + "particleDistributionFile" + i, 10);
+            } catch (IOException | NotEnoughSpaceException e) {
+                System.err.println(e);
+                return;
+            }
+
+            try {
+                String folder = "particles5000/";
+                TwoContainersSimulation tcs = new TwoContainersSimulation(200, 200, 5000,
+                        NUMBER_OF_ITERATIONS, folder + "staticFile", folder + "dynamicFile", folder + "particleDistributionFile" + i, 10);
+            } catch (IOException | NotEnoughSpaceException e) {
+                System.err.println(e);
+                return;
+            }
         }
 
     }
